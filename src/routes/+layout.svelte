84a0be7d2bbb4d767f5d93c7af1399852ad3a2d9
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-09-05 19:06:35 +0100

    hot fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-  	import "../global.css";
+	import favicon from '$lib/assets/logo.svg';
+	import '../global.css';
 	let { children } = $props();
 </script>
 
-
-
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Rafiki X</title>
+	<meta name="description" content="Rafiki X - AI powered chatbot" />
 </svelte:head>
 
 {@render children?.()}
-
-
